e13bf68c4044eae80304d5af1a1d3b42c91238e1
Wonbin Kim <kwb0711@gmail.com>
2025-05-07 06:57:10 +0800

    Modify kafka add queue

--- knowledge_base/core/component/add_queue/kafka.py
+++ knowledge_base/core/component/add_queue/kafka.py
@@ -1,15 +1,16 @@
 from knowledge_base.core.domain.port.add_queue import AddQueue
-from kafka import KafkaProducer, KafkaConsumer
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 from knowledge_base.core.domain.entity.knowledge import Knowledge
+from typing import Optional
 
 
 class KafkaAddQueue(AddQueue):
-    def __init__(self, producer: KafkaProducer, consumer: KafkaConsumer):
+    def __init__(self, producer: AIOKafkaProducer, consumer: AIOKafkaConsumer):
         self.producer = producer
         self.consumer = consumer
 
     async def put(self, knowledge: Knowledge):
-        self.producer.send(self.topic, knowledge)
+        await self.producer.send_and_wait(self.topic, knowledge)
 
     async def get(self) -> Knowledge:
-        return self.consumer.poll(timeout_ms=1000)
+        return await self.consumer.poll(timeout_ms=1000)
